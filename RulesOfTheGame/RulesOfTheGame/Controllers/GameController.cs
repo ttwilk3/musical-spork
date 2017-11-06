ae9680117b7f2f38d15c5c1b1aa8a057071923fa
Danerutherford <danerutherford94@gmail.com>
2017-11-06 10:39:37 -0500

    Pushing

--- RulesOfTheGame/RulesOfTheGame/Controllers/GameController.cs
+++ RulesOfTheGame/RulesOfTheGame/Controllers/GameController.cs
@@ -17,5 +17,10 @@ namespace RulesOfTheGame.Controllers
         {
             return View();
         }
+
+        public ActionResult Teetrees()
+        {
+            return View();
+        }
     }
 }
